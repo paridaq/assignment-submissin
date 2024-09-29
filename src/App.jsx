4baac5ede816e3fefeb8482ffd15a703be8459f7
Biswajit <paridabiswa2k@gmail.com>
2024-09-29 10:39:47 +0530

    adding comment in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,3 +27,11 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
+/* I built a fully functional application according to the guidelines using 
+Redux Toolkit, and all the functional components are working properly. However,
+ I wasn't able to complete the UI, and it doesn't look good. I also didn't implement
+  the routing functionality using React Router DOM because I couldn't get the UI right. */
